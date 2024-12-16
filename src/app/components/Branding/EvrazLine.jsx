3fb098adbb677b142e1bc5d0867cc885fe3e8d22
Konstantin Nered <konstantin.n616@gmail.com>
2024-12-17 01:06:44 +0700

    slightly lowered width of brand line

--- src/app/components/Branding/EvrazLine.jsx
+++ src/app/components/Branding/EvrazLine.jsx
@@ -1,6 +1,6 @@
 export default function EvrazLine(){
     return (
-        <div className={'w-2.5'}>
+        <div className={'w-2'}>
             <div className={'h-2.5 bg-[--yellow-color]'}></div>
             <div className={'h-2.5 bg-[--orange-color]'}></div>
             <div className={'h-2.5 bg-[--red-color]'}></div>
