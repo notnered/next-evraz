4aa54be3c637f9dcac292806373372ae05163a9e
Konstantin Nered <konstantin.n616@gmail.com>
2024-12-18 01:25:38 +0700

    created mainbutton component

---
+++ src/app/components/Buttons/MainButton.jsx
@@ -0,0 +1,11 @@
+export default function MainButton(props){
+    return (
+        <>
+            {props.full ? (
+                <button className="bg-[--orange-color] text-white px-3 py-1.5 font-medium rounded-sm text-lg transition-colors hover:bg-[--orange-hover-color] select-none w-full">{props.text}</button>
+            ) : (
+                <button className="bg-[--orange-color] text-white px-3 py-1.5 font-medium rounded-sm text-lg transition-colors hover:bg-[--orange-hover-color] select-none">{props.text}</button>
+            )}
+        </>
+    )
+}
