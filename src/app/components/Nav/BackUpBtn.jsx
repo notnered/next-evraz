6d0b421d70d63937392a4b946e87a9dcb5ac8485
Konstantin Nered <konstantin.n616@gmail.com>
2024-12-18 02:40:31 +0700

    did backup button

--- src/app/components/Nav/BackUpBtn.jsx
+++ src/app/components/Nav/BackUpBtn.jsx
@@ -1,9 +1,10 @@
 'use client';
+// REACT & REACT LIBS
 import { FaAngleUp } from "react-icons/fa6";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function BackUpBtn(){
-    // const [smoothScroll, setSmoothScroll] = useState(false);
+    let scrollThreshold = 0;
 
     function headToTop(){
         window.scrollTo({
@@ -12,10 +13,33 @@ export default function BackUpBtn(){
         });
     }
 
+    const [scrolledBy, setScrolledBy] = useState(0);
+    const [showBtn, setShowBtn] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            setScrolledBy(window.scrollY);
+            if (scrolledBy >= scrollThreshold){
+                setShowBtn(true);
+            } else {
+                setShowBtn(false);
+            }
+        })
+        const maxScroll = document.body.scrollHeight;
+        scrollThreshold = (maxScroll * 0.5) / 2;
+    })
 
     return (
-        <button onClick={headToTop} className="border-[1px] border-[--border-color] p-4 rounded-full">
-            <FaAngleUp style={{fontSize: '1.375rem'}} />
-        </button>
+        <>
+            {scrolledBy > scrollThreshold && showBtn ? (
+                <button onClick={headToTop} className="back-btn opacity-100 transition-[opacity] border-[1px] border-[--border-color] p-4 rounded-full fixed bottom-8 right-24">
+                    <FaAngleUp style={{fontSize: '1.375rem'}} />
+                </button>
+            ): (
+                <button onClick={headToTop} className="opacity-0 transition-[opacity] border-[1px] border-[--border-color] p-4 rounded-full fixed bottom-8 right-24">
+                <FaAngleUp style={{fontSize: '1.375rem'}} />
+            </button>
+            )}
+        </>
     )
 }
