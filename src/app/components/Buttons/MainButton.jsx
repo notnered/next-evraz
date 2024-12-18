7d7947f9e36e71a90cf885d6f793b12fb9a5e6e6
Konstantin Nered <konstantin.n616@gmail.com>
2024-12-19 00:44:26 +0700

    added ability to pass function to MainButton component

--- src/app/components/Buttons/MainButton.jsx
+++ src/app/components/Buttons/MainButton.jsx
@@ -1,10 +1,12 @@
 export default function MainButton(props){
+
+
     return (
         <>
             {props.full ? (
-                <button className="bg-[--orange-color] text-white px-3 py-1.5 font-medium rounded-sm text-lg transition-colors hover:bg-[--orange-hover-color] select-none w-full">{props.text}</button>
+                <button onClick={props.func} className="bg-[--orange-color] text-white px-3 py-1.5 font-medium rounded-sm text-lg transition-colors hover:bg-[--orange-hover-color] select-none w-full">{props.text}</button>
             ) : (
-                <button className="bg-[--orange-color] text-white px-3 py-1.5 font-medium rounded-sm text-lg transition-colors hover:bg-[--orange-hover-color] select-none">{props.text}</button>
+                <button onClick={props.func} className="bg-[--orange-color] text-white px-3 py-1.5 font-medium rounded-sm text-lg transition-colors hover:bg-[--orange-hover-color] select-none">{props.text}</button>
             )}
         </>
     )
