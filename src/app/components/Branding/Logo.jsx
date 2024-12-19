59eb2e6bed13530c1a10d71f766af18a95a59c3c
Konstantin Nered <konstantin.n616@gmail.com>
2024-12-20 01:49:58 +0700

    fixed logo shrinking

--- src/app/components/Branding/Logo.jsx
+++ src/app/components/Branding/Logo.jsx
@@ -4,7 +4,7 @@ import logo from '../../../../public/logo.svg';
 export default function Logo(){
     return (
         <div className='flex w-fit'>
-            <img src={logo.src} alt='ЕВРАЗ-ЗСМК' className='w-72 ml-8 lg:ml-0' width={72} height={72}/>
+            <img src={logo.src} alt='ЕВРАЗ-ЗСМК' className='min-w-64 w-72' width={72} height={72}/>
         </div>
     )
 }
