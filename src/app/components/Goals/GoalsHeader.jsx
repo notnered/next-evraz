8da37340fb547fb63f9dcc7d67e80073e041341b
Konstantin Nered <konstantin.n616@gmail.com>
2024-12-22 01:15:29 +0700

    changed top position to be more suitable for mobile devices

--- src/app/components/Goals/GoalsHeader.jsx
+++ src/app/components/Goals/GoalsHeader.jsx
@@ -7,7 +7,7 @@ export default function GoalsHeader(props){
 
     return (
         <div className="row-span-2 relative">
-            <div className='absolute right-0 flex top-24 px-4 py-8 bg-[--orange-color]'>
+            <div className='absolute right-0 flex top-8 sm:top-16 lg:top-24 px-4 py-8 bg-[--orange-color]'>
                 <h1 className='text-white text-4xl font-bold'>Цели и задачи</h1>
             </div>
             <Image src={goalsHeader.src} alt="Goals Header" width={goalsHeader.width} height={goalsHeader.height} className='h-full max-h-[30em] w-full object-cover border-[1px] border-[--border-color]' />
