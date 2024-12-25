6fb017b3b3064ee10ccf1160ff63f13850a6e5b5
Konstantin Nered <konstantin.n616@gmail.com>
2024-12-26 04:29:53 +0700

    changed query request in unique one

--- src/app/catalog/[category]/page.jsx
+++ src/app/catalog/[category]/page.jsx
@@ -3,13 +3,21 @@ import CatalogItems from "@/app/components/Catalog/CatalogItems";
 import Hero from "@/app/components/Hero/Hero";
 import MainLayout from "@/app/components/Layouts/MainLayout";
 // DATA
+import { queryCategory, queryOneCategory } from "@/database/getQueryOutput";
 
-export default async function CatalogCategory(){
+export default async function CatalogCategory({params}){
+    const category_id = await params;
+    const options = {
+        where: {
+            id: parseInt(category_id.category),
+        }
+    }
+    const data = await queryOneCategory(options);
     return (
         <>
             <MainLayout>
                 <Hero />
-                <CatalogItems />
+                <CatalogItems category={data} />
             </MainLayout>
         </>
     )
