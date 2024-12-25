ffcb19bab312000b207ca7a25951c52772c66399
Konstantin Nered <konstantin.n616@gmail.com>
2024-12-26 05:16:47 +0700

    updated catalog pages

--- src/app/catalog/[category]/[id]/page.jsx
+++ src/app/catalog/[category]/[id]/page.jsx
@@ -1,5 +1,18 @@
-export default function CatalogItem(){
+// COMPONENTS
+import CatalogItems from "@/app/components/Catalog/CatalogItems";
+import Hero from "@/app/components/Hero/Hero";
+import MainLayout from "@/app/components/Layouts/MainLayout";
+// DATA
+import { queryCategory, queryOneCategory } from "@/database/getQueryOutput";
+
+
+export default async function CatalogItem(){
     return (
-        1
+        <>
+            <MainLayout>
+                <Hero />
+                <CatalogItems category={data} />
+            </MainLayout>
+        </>
     )
 }
--- src/app/components/Catalog/CatalogPage.jsx
+++ src/app/components/Catalog/CatalogPage.jsx
@@ -1,36 +1,57 @@
 'use client';
 // REACT & REACT LIBS
 import { useRouter } from "next/navigation";
+import { useState } from 'react';
 // COMPONENTs
 import EvrazLine from "../Branding/EvrazLine";
 import Image from "next/image";
 import temp from "../../../../public/sphere/sphere2.jpg";
+import MainButton from "../Buttons/MainButton";
+import ContactModal from "../Contact/ContactModal";
 // DATA
 
 export default function CatalogPage(props){
     const router = useRouter();
     const categoriesArray = props.catalogData;
 
+    const [modal, setModal] = useState(false);
+
+    function switchModal(){
+        setModal(!modal);
+    }
+
+    function sendMessage(){
+        switchModal();
+    }
+
     return (
-        <div className="my-8 px-2">
-            <div className="flex justify-center pb-4">
-                <h1 className="font-semibold text-2xl">Категории продукции</h1>
-            </div>
-            <div className="grid grid-cols-1 lg:grid-cols-3 grid-flow-row gap-4 lg:gap-y-8 lg:gap-x-8">
+        <>
+            {modal ? (
+                <ContactModal closeFunc={switchModal} sendFunc={sendMessage} />
+            ) : null}
+            <div className="my-8 px-2">
+                <div className="flex justify-center pb-4">
+                    <h1 className="font-semibold text-2xl">Категории продукции</h1>
+                </div>
+                <div className="grid grid-cols-1 lg:grid-cols-3 grid-flow-row gap-4 lg:gap-y-8 lg:gap-x-8">
 
-            {categoriesArray.map((item) => {
-                return (
-                    <div onClick={() => router.push(`/catalog/${item.id}`)} key={item.id} className="border-[1px] group border-[--border-color] px-4 pt-4 pb-2 relative cursor-pointer">
-                        <span className="absolute left-0"><EvrazLine /></span>
-                        <h1 className="font-medium text-2xl">{item.title}</h1>
-                        <div className="py-4">
-                            <Image src={temp.src} className="rounded-sm w-full transition-opacity duration-200 group-hover:opacity-90" alt={'Image'} width={temp.width} height={temp.height}></Image>
-                        </div>                
-                    </div>
-                )
-            })}
+                {categoriesArray.map((item) => {
+                    return (
+                        <div key={item.id} className="border-[1px] border-[--border-color] px-4 pt-4 pb-4 relative flex flex-col">
+                            <span className="absolute left-0"><EvrazLine /></span>
+                            <h1 className="font-medium text-2xl pb-2">{item.title}</h1>
+                            <div>
+                                <p className="line-clamp-[8]">{item.desc}</p>
+                            </div>
+                            <div className="mt-auto pt-2">
+                                <MainButton text={'Оставить заявку'} func={() => router.push('/contact')} />
+                            </div>
+                        </div>
+                    )
+                })}
 
+                </div>
             </div>
-        </div>
+        </>
     )
 }
