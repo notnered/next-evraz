be9cbaf366c986f7aa44970e1224bb0b0016c87c
Konstantin Nered <konstantin.n616@gmail.com>
2024-12-26 19:55:45 +0700

    created news date filter

--- src/app/components/News/NewsDate.jsx
+++ src/app/components/News/NewsDate.jsx
@@ -1,24 +1,56 @@
 // COMPONENTS
+import { FaChevronCircleRight } from 'react-icons/fa';
 import LongEvrazLine from '../Branding/LongEvrazLine';
 import MainButton from '../Buttons/MainButton';
 // DATA
+import { queryNews } from '@/database/getQueryOutput';
 // import newsPosts from "./newsPosts";
-const postsCount = [
-    { month: 'Декабрь', count: 2 },
-    { month: 'Ноябрь', count: 7 },
-    { month: 'Октябрь', count: 1 },
-    { month: 'Сентябрь', count: 2 },
-    { month: 'Август', count: 2 },
-    { month: 'Июль', count: 3 },
-    { month: 'Июнь', count: 6 },
-    { month: 'Май', count: 3 },
-    { month: 'Апрель', count: 1 },
-    { month: 'Март', count: 3 },
-    { month: 'Февраль', count: 4 },
-    { month: 'Январь', count: 5 },
+const monthsList = [
+    'Январь',
+    'Февраль',
+    'Март', 
+    'Апрель', 
+    'Май', 
+    'Июнь', 
+    'Июль',
+    'Август',
+    'Сентябрь',
+    'Октябрь', 
+    'Ноябрь',
+    'Декабрь',
 ];
 
-export default function NewsDate() {
+export default async function NewsDate() {
+    const dates = [];
+    const datesObjects = [];
+    const data = await queryNews();
+    data.forEach((item) => {
+        const date = item.createdAt.toLocaleDateString('ru-ru').split('.');
+        const month = parseInt(date[1]);
+        dates.push(month);
+    })
+    function updateCount() {
+        for (let i = 0; i < dates.length; i++) {
+            const monthIndex = dates[i];
+            const existingObj = datesObjects.find(obj => {
+                return obj.month === monthsList[monthIndex - 1]
+            });
+            if (existingObj) {
+                existingObj.count++;
+            } else {
+                const dateObj = {
+                    month: monthsList[monthIndex - 1],
+                    count: 1,
+                };
+                datesObjects.push(dateObj);
+            }
+        }
+    }
+
+    updateCount();
+
+    console.log(datesObjects);
+
     return (
         <div className='flex flex-col'>
             <div className='border-[1px] border-[--border-color] h-fit'>
@@ -26,7 +58,7 @@ export default function NewsDate() {
                 <div className='px-2 py-2'>
                     <h1 className='text-xl font-medium'>Новости за 2024 год</h1>
                     <ul className='flex flex-col gap-y-0.5'>
-                        {postsCount.map((post, index) => {
+                        {datesObjects.map((post, index) => {
                             return (
                                 <li
                                     className='text-xl font-medium transition-colors hover:text-[--orange-color] cursor-pointer w-fit select-none'
