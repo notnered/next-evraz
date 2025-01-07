ca71a1303310ad161777baebc13277c924fbb44c
Konstantin Nered <konstantin.n616@gmail.com>
2025-01-08 01:12:37 +0700

    updated newsdate, todo: make smooth loading for it too

--- src/app/components/News/NewsDate.jsx
+++ src/app/components/News/NewsDate.jsx
@@ -3,10 +3,9 @@
 import { FaChevronCircleRight } from 'react-icons/fa';
 import LongEvrazLine from '../Branding/LongEvrazLine';
 import MainButton from '../Buttons/MainButton';
-// DATA
-import { queryNews } from '@/database/getQueryOutput';
 import { useState, useEffect } from 'react';
-// import newsPosts from "./newsPosts";
+import { PulseLoader } from 'react-spinners';
+// DATA
 const monthsList = [
     'Январь',
     'Февраль',
@@ -23,61 +22,53 @@ const monthsList = [
 ];
 
 export default function NewsDate() {
-    // const dates = [];
-    // const datesObjects = [];
-
+    const datesObjects = [];
+    const [dates, setDates] = useState([]);
     const [news, setNews] = useState([]);
+    const [loading, setLoading] = useState(false);
     
-        useEffect(() => {
-            const getNews = async () => {
-                const data = await fetch('/api/news');
-                const jsondata = await data.json();
-                setNews([...jsondata]);
+    useEffect(() => {
+        const getNews = async () => {
+            const data = await fetch('/api/news');
+            const jsondata = await data.json();
+            setNews([...jsondata]);
+        }
+        getNews();
+    }, [])
+
+    // console.log('news', news);
+
+    useEffect(() => {
+        const loopDates = [];
+        news.forEach((item) => {
+            const date = new Date(item.createdAt).toLocaleDateString('ru-ru').split('.');
+            const month = parseInt(date[1]);
+            loopDates.push(month);
+        });
+        setDates(loopDates);
+    }, [news]);
+
+    // console.log(dates);
+
+    function updateCount() {
+        for (let i = 0; i < dates.length; i++) {
+            const monthIndex = dates[i];
+            const existingObj = datesObjects.find(obj => {
+                return obj.month === monthsList[monthIndex - 1]
+            });
+            if (existingObj) {
+                existingObj.count++;
+            } else {
+                const dateObj = {
+                    month: monthsList[monthIndex - 1],
+                    count: 1,
+                };
+                datesObjects.push(dateObj);
             }
-            getNews();
-        }, [])
+        }
+    }
 
-        console.log(news);
-
-        // console.log(dates);
-    // function getNews(){
-    //     let newsdata;
-    //     const response = fetch('/api/news', {
-    //         method: 'GET',
-    //     });
-    //     response.then((data) => {
-    //         const jsondata = data.json();
-    //         return jsondata;
-    //     })
-    // }
-    
-    // const data = getNews();
-    // console.log(data);
-
-    // dates.forEach((item) => {
-    //     const date = new Date(item.createdAt).toLocaleDateString('ru-ru').split('.');
-    //     const month = parseInt(date[1]);
-    //     dates.push(month);
-    // })
-    // function updateCount() {
-    //     for (let i = 0; i < dates.length; i++) {
-    //         const monthIndex = dates[i];
-    //         const existingObj = datesObjects.find(obj => {
-    //             return obj.month === monthsList[monthIndex - 1]
-    //         });
-    //         if (existingObj) {
-    //             existingObj.count++;
-    //         } else {
-    //             const dateObj = {
-    //                 month: monthsList[monthIndex - 1],
-    //                 count: 1,
-    //             };
-    //             datesObjects.push(dateObj);
-    //         }
-    //     }
-    // }
-
-    // updateCount();
+    updateCount();
 
     return (
         <div className='flex flex-col'>
@@ -86,16 +77,26 @@ export default function NewsDate() {
                 <div className='px-2 py-2'>
                     <h1 className='text-xl font-medium'>Новости за 2024 год</h1>
                     <ul className='flex flex-col gap-y-0.5'>
-                        {/* {datesObjects.map((post, index) => {
-                            return (
-                                <li
-                                    className='text-xl font-medium transition-colors hover:text-[--orange-color] cursor-pointer w-fit select-none'
-                                    key={index}
-                                >
-                                    {post.month} ({post.count})
-                                </li>
-                            );
-                        })} */}
+                        {loading ? (
+                            <div className='my-2'>
+                                <PulseLoader
+                                    color='#F0862F'
+                                    size={12}
+                                    loading={loading}
+                                />
+                            </div>
+                        ) : (
+                            datesObjects.map((post, index) => {
+                                return (
+                                    <li
+                                        className='text-xl font-medium transition-colors hover:text-[--orange-color] cursor-pointer w-fit select-none'
+                                        key={index}
+                                    >
+                                        {post.month} ({post.count})
+                                    </li>
+                                );
+                            })
+                        )}
                     </ul>
                 </div>
             </div>
