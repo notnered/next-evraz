c22f9d83049afba6e6dae03eb6f657877366843f
Konstantin Nered <konstantin.n616@gmail.com>
2025-01-12 16:31:11 +0700

    returned to server component on main page to remove unnecessary server load

--- src/app/components/News/NewsBlock.jsx
+++ src/app/components/News/NewsBlock.jsx
@@ -1,56 +1,30 @@
-'use client';
-import { useState, useEffect } from 'react';
 // COMPONENTS
 import NewsSection from "./NewsSection";
-import { PulseLoader } from 'react-spinners';
 // DATA
+import { queryNews } from "@/database/getQueryOutput";
 
-export default function NewsBlock(){
+export default async function NewsBlock(){
     const queryOptions = {
         skip: 0,
         take: 3,
     };
 
-    const [news, setNews] = useState([]);
-    const placeholderNews = Array.from(Array(queryOptions.take).keys());
-    const [loading, setLoading] = useState(true);
-    
-    useEffect(() => {
-        async function getNews(){
-            setLoading(true);
-            const data = await fetch(`/api/news`);
-            const jsondata = await data.json();
-            setNews(jsondata.slice(queryOptions.skip, queryOptions.take));
-            setLoading(false);
-        }
-
-        getNews();
-    }, []);
-
-    // console.log(news);
+    const news = await queryNews(queryOptions);
 
 
     return (
         <div className="grid grid-cols-1 lg:grid-cols-3 px-4 lg:px-2 gap-y-[1.25rem] gap-x-[2.5rem] my-8">
-            { loading ? (
-                placeholderNews.map((item, index) => {
-                    return (
-                        <div className='flex justify-center'  key={index}>
-                            <PulseLoader
-                                color='#F0862F'
-                                size={16}
-                                loading={loading}
-                            />
-                        </div>
-                    )
-                })
-            ) : (
-                news.map((news) => {
-                    return (
-                        <NewsSection key={news.id} id={news.id} title={news.title} date={new Date(news.createdAt).toLocaleDateString('ru-ru')} full={false} />
-                    )
-                })
-            )}
+            {news.map((news) => {
+                return (
+                    <NewsSection 
+                        key={news.id} 
+                        id={news.id} 
+                        title={news.title} 
+                        date={new Date(news.createdAt).toLocaleDateString('ru-ru')} 
+                        full={false} 
+                    />
+                )
+            })}
         </div>
     )
 }
